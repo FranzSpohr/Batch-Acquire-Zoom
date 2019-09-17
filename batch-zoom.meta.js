072519911b756883bc3f2c6e51fe673d725aa3c9
FranzSpohr <55456842+FranzSpohr@users.noreply.github.com>
2019-09-17 15:20:46 -0400

    Create batch-zoom.meta.js

---
+++ batch-zoom.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Batch Acquire Zoom
+// @namespace    http://tampermonkey.net/
+// @version      2.5
+// @description  For Slate Batch Acquire; zoom in and out with "+" and "-" keys on the numberpad. Needs Tampermonkey or Greasemonkey
+// @author       Theodore Ma
+// @match        https://*/manage/database/acquire
+// @match        https://*/manage/lookup/* 
+// @updateURL    https://github.com/FranzSpohr/Batch-Acquire-Zoom/blob/master/batch-zoom.user.js
+// @grant        none
+// ==/UserScript==
