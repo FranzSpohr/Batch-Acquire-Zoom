56c8f0c12869412ac34062f7a3d8ed51360bef2b
FranzSpohr <55456842+FranzSpohr@users.noreply.github.com>
2019-09-25 13:28:29 -0400

    Update batch-zoom.user.js

--- batch-zoom.user.js
+++ batch-zoom.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Batch Acquire Zoom
 // @namespace    http://tampermonkey.net/
-// @version      2.7
-// @description  For Slate Batch Acquire; zoom-in with "=" and zoom-out with "-" on your keyboard next to the number row. "+" and "-" keys on the numberpad is also an option. Needs Tampermonkey for Chrome or Greasemonkey for Firefox. 
+// @version      3.0
+// @description  For Slate Batch Acquire; to zoom-in: press left mouse click, "=", or "+" on numpad. To zoom-out: press right mouse click, "-", or "-" on numpad. Needs Tampermonkey for Chrome or Greasemonkey for Firefox. 
 // @author       University of Michigan OUA Processing (Theodore Ma)
 // @match        https://*/manage/database/acquire
 // @match        https://*/manage/lookup/* 
@@ -10,44 +10,56 @@
 // @grant        none
 // ==/UserScript==
 
-window.addEventListener('keydown', ZoomIn);
-window.addEventListener('keydown', ZoomOut);
-var ZOOMED_DPI = 72;
-var ZOOMED_DPI1 = 108;
-var ZOOMED_DPI2 = 144;
-var ZOOMED_DPI3 = 180;
-var ZOOMED_DPI4 = 216;
+var i = 0
+var Zoom_Levels = [72, 108, 144, 180, 216];
 
-function ZoomIn (event) {
-    if (event.code == 'NumpadAdd'||event.code == 'Equal') {
+window.addEventListener('keydown', Toggle_Zoom, true)
+window.addEventListener('click', Add_Listener, true)
+
+function Toggle_Zoom (event) {
+    if (event.code == 'NumpadAdd'||event.code == 'Equal'||event.type == 'click') {
+        event.preventDefault();
+        if (i == 4) {return};
         const elements = document.querySelectorAll('.batch_page_container > img');
         elements.forEach(function(el) {
-            if (el.src.includes(`z=${ZOOMED_DPI}`)) {
-                el.src = el.src.replace(`z=${ZOOMED_DPI}`, `z=${ZOOMED_DPI1}`);
-            } else if (el.src.includes(`z=${ZOOMED_DPI1}`)) {
-                el.src = el.src.replace(`z=${ZOOMED_DPI1}`, `z=${ZOOMED_DPI2}`);
-            } else if (el.src.includes(`z=${ZOOMED_DPI2}`)) {
-                el.src = el.src.replace(`z=${ZOOMED_DPI2}`, `z=${ZOOMED_DPI3}`);
-            } else if (el.src.includes(`z=${ZOOMED_DPI3}`)) {
-                el.src = el.src.replace(`z=${ZOOMED_DPI3}`, `z=${ZOOMED_DPI4}`);
+            if (el.src.includes(`z=${Zoom_Levels[i]}`)) {
+                el.src = el.src.replace(`z=${Zoom_Levels[i]}`, `z=${Zoom_Levels[i+1]}`);
             }
         });
+        i++;
+        var targetNode = document.getElementsByClassName('batch_zoomer boxshadow')[0];
+        if (targetNode) {
+            //--- Simulate a natural mouse-click sequence.
+            triggerMouseEvent (targetNode, "mousedown");
+            triggerMouseEvent (targetNode, "mouseup");
+            triggerMouseEvent (targetNode, "click");
+        }
+        else {
+            console.log ("*** Target node not found!");
+        }
+
+        function triggerMouseEvent (node, eventType) {
+            var clickEvent = document.createEvent ('MouseEvents');
+            clickEvent.initEvent (eventType, true, true);
+            node.dispatchEvent (clickEvent);
+        }
+    } else if (event.code == 'NumpadSubtract'||event.code == 'Minus'||event.type == 'contextmenu') {
+        event.preventDefault();
+        if (i == 0) {return};
+        const elements = document.querySelectorAll('.batch_page_container > img');
+        elements.forEach(function(el) {
+            if (el.src.includes(`z=${Zoom_Levels[i]}`)) {
+                el.src = el.src.replace(`z=${Zoom_Levels[i]}`, `z=${Zoom_Levels[i-1]}`);
+            }
+        });
+        i--;
     }
 }
 
-function ZoomOut (event) {
-    if (event.code == 'NumpadSubtract'||event.code == 'Minus') {
-        const elements = document.querySelectorAll('.batch_page_container > img');
-        elements.forEach(function(el) {
-            if (el.src.includes(`z=${ZOOMED_DPI4}`)) {
-                el.src = el.src.replace(`z=${ZOOMED_DPI4}`, `z=${ZOOMED_DPI3}`);
-            } else if (el.src.includes(`z=${ZOOMED_DPI3}`)) {
-                el.src = el.src.replace(`z=${ZOOMED_DPI3}`, `z=${ZOOMED_DPI2}`);
-            } else if (el.src.includes(`z=${ZOOMED_DPI2}`)) {
-                el.src = el.src.replace(`z=${ZOOMED_DPI2}`, `z=${ZOOMED_DPI1}`);
-            } else if (el.src.includes(`z=${ZOOMED_DPI1}`)) {
-                el.src = el.src.replace(`z=${ZOOMED_DPI1}`, `z=${ZOOMED_DPI}`);
-            }
-        });
-    }
+function Add_Listener () {
+    const elements = document.querySelectorAll('.batch_page_container > img');
+    elements.forEach(function(el) {
+        el.addEventListener('click', Toggle_Zoom, true)
+        el.addEventListener('contextmenu', Toggle_Zoom, true)
+    })
 }
