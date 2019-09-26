17c66b90a504708e5ab39c037b937f5f8f6a3f06
FranzSpohr <55456842+FranzSpohr@users.noreply.github.com>
2019-09-26 12:40:14 -0400

    Update batch_zoom.user.js

--- batch_zoom.user.js
+++ batch_zoom.user.js
@@ -2,7 +2,7 @@
 // @name         Batch Acquire Zoom
 // @namespace    http://tampermonkey.net/
 // @version      4.0.1
-// @description  For Slate Batch Acquire; to zoom-in: press left mouse click, "=", or "+" on numpad. To zoom-out: press right mouse click, "-", or "-" on numpad. Needs Tampermonkey for Chrome or Greasemonkey for Firefox. Based on code by Jamie Davis @ UMich.
+// @description  For Slate Batch Acquire. Needs Tampermonkey for Chrome or Greasemonkey for Firefox. See readme for more info. 
 // @author       University of Michigan OUA Processing (Theodore Ma)
 // @match        https://*/manage/database/acquire
 // @match        https://*/manage/lookup/*
