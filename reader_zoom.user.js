6be52d951f012d0bd677b4401d421b4fd5fefb7c
FranzSpohr <55456842+FranzSpohr@users.noreply.github.com>
2019-10-09 14:53:59 -0400

    Update reader_zoom.user.js

--- reader_zoom.user.js
+++ reader_zoom.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Slate Reader Zoom
 // @namespace    http://tampermonkey.net/
-// @version      1.3.1
+// @version      10.9.19
 // @description  For Slate Reader. Opens a page with a highder DPI render of document. Needs Tampermonkey for Chrome or Greasemonkey for Firefox.
 // @author       University of Michigan OUA Processing (Theodore Ma)
 // @match        https://*/manage/reader/*
@@ -44,18 +44,26 @@ if (/manage\/reader/.test (location.pathname) ) {
 // Run code for all sites here.
 function zoom_Overlay(){
     const imageLink = document.querySelector("body > div.reader_viewer.reader_scrollable > div > div.container.active.loaded > div > img");
-    var startPage = 1
-    var currentPage = document.getElementsByClassName('reader_status')[0].childNodes[0].textContent.match(/\d+/);
-    var endPage = document.getElementsByClassName('reader_status')[0].childNodes[0].textContent.match(/\d+(?=,)/);
     if (imageLink == null) {
         alert("Navigate to a tab with documents first.");
         return;
     } else if (imageLink.src.includes('z=72')||imageLink.src.includes('z=96')||imageLink.src.includes('z=144')){
         imageLink.src = imageLink.src.replace(/z=\d*/, 'z=300');
+        var startPage = 1;
+        var currentPage = document.getElementsByClassName('reader_status')[0].childNodes[0].textContent.match(/\d+/);
+        var endPage = document.getElementsByClassName('reader_status')[0].childNodes[0].textContent.match(/\d+(?=,)/);
     }
     imageLink.src = imageLink.src.replace(/pg=\d*/, `pg=${currentPage-1}`);
-    window.open(imageLink.src, "_blank", 'height=' + winHeight + ',width=' + winWidth +',toolbar=no,resiable=no,menubar=no,location=no,status=no,scrollbars=no').moveTo(0,0);
+    var win = popupWindow("","Zoom",window,1920,1080)
+    win.document.body.innerHTML = '<img class = "zoomed_image" src=' + imageLink.src + ' width = "100%" height="auto">'
 };
+
+function popupWindow(url, title, win, w, h) {
+    const y = win.top.outerHeight / 2 + win.top.screenY - ( h / 2);
+    const x = win.top.outerWidth / 2 + win.top.screenX - ( w / 2);
+    return win.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+y+', left='+x);
+}
+
 
 function closeWindow(){
     window.close()
