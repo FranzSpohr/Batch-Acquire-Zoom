bbbd8e6e59b361bf07c2379e906a4a2a188b85c1
FranzSpohr <55456842+FranzSpohr@users.noreply.github.com>
2019-10-10 13:51:06 -0400

    Update reader_zoom.user.js

--- reader_zoom.user.js
+++ reader_zoom.user.js
@@ -1,70 +1,66 @@
 // ==UserScript==
-// @name         Slate Reader Zoom
+// @name         Slate Reader Zoom Test
 // @namespace    http://tampermonkey.net/
-// @version      10.9.19
-// @description  For Slate Reader. Opens a page with a highder DPI render of document. Needs Tampermonkey for Chrome or Greasemonkey for Firefox.
+// @version      10.10.19
+// @description  For Slate Reader. Opens an overlay with a highder DPI render of document. Needs Tampermonkey for Chrome or Greasemonkey for Firefox.
 // @author       University of Michigan OUA Processing (Theodore Ma)
 // @match        https://*/manage/reader/*
 // @match        https://api.cdn.technolutions.net/pdf/*
 // @updateURL    https://github.com/FranzSpohr/Slate_Tools/blob/master/reader_zoom.user.js
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
 
-// little buffer to prevent forced scrollbars
-var padding = 20;
-
-// Values to use when opening window
-var winWidth = screen.width + padding;
-var winHeight = screen.height + padding;
-
-if (/manage\/reader/.test (location.pathname) ) {
-    //Run in Slate Reader
-    var input=document.createElement("input");
-
-    input.type="button";
-    input.value="Open Zoomed-In Image";
-    input.onclick = zoom_Overlay;
-    input.setAttribute("style", "font-size:14px;position:absolute;top:97.4%;right:62.5%;");
-    document.body.appendChild(input);
-} else if (/pdf\/render/.test (location.pathname) ) {
-    //Run in the new window
-    const newImg = document.querySelector("img");
-
-    newImg.style.width = (screen.width)*0.975;
-    newImg.style.height = 'auto';
-
-    var buttonExit=document.createElement("input");
-    buttonExit.type="button";
-    buttonExit.value="Exit";
-    buttonExit.onclick = closeWindow;
-    buttonExit.setAttribute("style", "font-size:14px;position:fixed;top:96%;right:50%;")
-    document.body.appendChild(buttonExit);
+GM_addStyle (`
+#overlay {
+overflow: auto;
+position: fixed;
+display: none;
+width: 100%;
+height: 100%;
+top: 0;
+left: 0;
+right: 0;
+bottom: 0;
+background-color: rgba(0,0,0,0.5);
+z-index: 2;
+cursor: pointer;
 }
+`);
 
-// Run code for all sites here.
-function zoom_Overlay(){
+var overlay=document.createElement("div");
+
+overlay.innerHTML = '<div id="overlay"></div>;';
+overlay.onclick = overlayOff;
+
+document.body.appendChild(overlay);
+
+var input=document.createElement("input");
+
+input.type="button";
+input.value="Display Larger Image";
+input.onclick = overlayOn;
+input.setAttribute("style", "font-size:14px;position:absolute;top:97.4%;right:62.5%;");
+
+document.body.appendChild(input);
+
+function overlayOn() {
     const imageLink = document.querySelector("body > div.reader_viewer.reader_scrollable > div > div.container.active.loaded > div > img");
     if (imageLink == null) {
         alert("Navigate to a tab with documents first.");
         return;
-    } else if (imageLink.src.includes('z=72')||imageLink.src.includes('z=96')||imageLink.src.includes('z=144')){
+    } else {
         imageLink.src = imageLink.src.replace(/z=\d*/, 'z=300');
-        var startPage = 1;
         var currentPage = document.getElementsByClassName('reader_status')[0].childNodes[0].textContent.match(/\d+/);
+        var startPage = 1;
         var endPage = document.getElementsByClassName('reader_status')[0].childNodes[0].textContent.match(/\d+(?=,)/);
+        imageLink.src = imageLink.src.replace(/pg=\d*/, `pg=${currentPage-1}`);
+        document.getElementById("overlay").style.display = "block";
+        document.getElementById("overlay").innerHTML = '<img class = "zoomed_image" src=' + imageLink.src + 'position = "fixed" width = "100%" height="auto">;'
+        alert(imageLink.src)
+        alert(currentPage)
     }
-    imageLink.src = imageLink.src.replace(/pg=\d*/, `pg=${currentPage-1}`);
-    var win = popupWindow("","Zoom",window,1920,1080)
-    win.document.body.innerHTML = '<img class = "zoomed_image" src=' + imageLink.src + ' width = "100%" height="auto">'
-};
-
-function popupWindow(url, title, win, w, h) {
-    const y = win.top.outerHeight / 2 + win.top.screenY - ( h / 2);
-    const x = win.top.outerWidth / 2 + win.top.screenX - ( w / 2);
-    return win.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+y+', left='+x);
 }
 
-
-function closeWindow(){
-    window.close()
+function overlayOff() {
+  document.getElementById("overlay").style.display = "none";
 }
