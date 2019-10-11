dd6fe5d4a9a3cb1a959d5711b876b227efb6b654
FranzSpohr <55456842+FranzSpohr@users.noreply.github.com>
2019-10-11 09:57:17 -0400

    Update reader_zoom.user.js

--- reader_zoom.user.js
+++ reader_zoom.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Slate Reader Zoom
+// @name         Slate Reader Zoom Test
 // @namespace    http://tampermonkey.net/
-// @version      10.10.19
-// @description  For Slate Reader. Opens an overlay with a highder DPI render of document. Needs Tampermonkey for Chrome or Greasemonkey for Firefox.
+// @version      10.11.19
+// @description  For Slate Reader. Opens a page with a highder DPI render of document. Needs Tampermonkey for Chrome or Greasemonkey for Firefox.
 // @author       University of Michigan OUA Processing (Theodore Ma)
 // @match        https://*/manage/reader/*
 // @match        https://api.cdn.technolutions.net/pdf/*
@@ -11,6 +11,8 @@
 // ==/UserScript==
 
 GM_addStyle (`
+.mySlides {display: none}
+
 #overlay {
 overflow: auto;
 position: fixed;
@@ -25,22 +27,75 @@ background-color: rgba(0,0,0,0.5);
 z-index: 2;
 cursor: pointer;
 }
+
+.prev, .next {
+cursor: pointer;
+position: fixed;
+top: 50%;
+width: auto;
+margin-top: -22px;
+padding: 16px;
+color: black;
+font-weight: bold;
+font-size: 18px;
+transition: 0.6s ease;
+border-radius: 0 3px 3px 0;
+user-select: none;
+}
+
+.next {
+right: 1%;
+border-radius: 3px 0 0 3px;
+}
+
+.prev:hover, .next:hover {
+background-color: rgba(0,0,0,0.8);
+color: white;
+}
+
+.numbertext {
+color: 	#000000;
+font-size: 25px;
+padding: 8px 12px;
+position: fixed;
+top: 0;
+background-color: #FEFEFE;
+}
+
+.active, .dot:hover {
+background-color: #717171;
+}
+
+.fade {
+-webkit-animation-name: fade;
+-webkit-animation-duration: 1.5s;
+animation-name: fade;
+animation-duration: 1.5s;
+}
+
+@-webkit-keyframes fade {
+from {opacity: .4}
+to {opacity: 1}
+}
+
+@keyframes fade {
+from {opacity: .4}
+to {opacity: 1}
+}
 `);
 
-var overlay=document.createElement("div");
+var slideIndex = 1
+var imageLoaded = false
 
-overlay.innerHTML = '<div id="overlay"></div>;';
-overlay.onclick = overlayOff;
-
+var overlay = document.createElement("div");
+overlay.id = 'overlay'
 document.body.appendChild(overlay);
 
 var input=document.createElement("input");
-
 input.type="button";
 input.value="Display Larger Image";
 input.onclick = overlayOn;
 input.setAttribute("style", "font-size:14px;position:absolute;top:97.4%;right:62.5%;");
-
 document.body.appendChild(input);
 
 function overlayOn() {
@@ -48,17 +103,72 @@ function overlayOn() {
     if (imageLink == null) {
         alert("Navigate to a tab with documents first.");
         return;
+    } else if (imageLoaded) {
+        document.getElementById("overlay").style.display = "block";
+        slideIndex = 1;
+        showSlides(slideIndex);
+        return;
     } else {
         imageLink.src = imageLink.src.replace(/z=\d*/, 'z=300');
         var currentPage = document.getElementsByClassName('reader_status')[0].childNodes[0].textContent.match(/\d+/);
         var startPage = 1;
         var endPage = document.getElementsByClassName('reader_status')[0].childNodes[0].textContent.match(/\d+(?=,)/);
-        imageLink.src = imageLink.src.replace(/pg=\d*/, `pg=${currentPage-1}`);
         document.getElementById("overlay").style.display = "block";
-        document.getElementById("overlay").innerHTML = '<img class = "zoomed_image" src=' + imageLink.src + 'position = "fixed" width = "100%" height="auto">;'
+
+        for (var i = startPage; i <= endPage; i++) {
+            imageLink.src = imageLink.src.replace(/pg=\d*/, `pg=${i-1}`);
+            var slide = document.createElement("div")
+            slide.id = 'slide'+i
+            slide.className = 'mySlides fade'
+            document.getElementById("overlay").appendChild(slide)
+            var pgCounter = document.createElement("div")
+            pgCounter.className = "numbertext"
+            pgCounter.innerHTML = i+'/'+endPage
+            document.getElementById("slide"+i).appendChild(pgCounter)
+            var imageLoc = document.createElement("img")
+            imageLoc.src = imageLink.src
+            imageLoc.onclick = overlayOff
+            imageLoc.style.width = '100%'
+            document.getElementById("slide"+i).appendChild(imageLoc)
+        }
+
+        var forward = document.createElement("a");
+        forward.className = "next"
+        forward.onclick = plusSlides
+        forward.innerHTML = "&#10095;"
+        document.getElementById("overlay").appendChild(forward);
+
+        var backward = document.createElement("a");
+        backward.className = "prev"
+        backward.onclick = minusSlides
+        backward.innerHTML = "&#10094;"
+        document.getElementById("overlay").appendChild(backward);
+
+        slideIndex = 1;
+        showSlides(slideIndex);
+        imageLoaded = true
     }
 }
 
 function overlayOff() {
-  document.getElementById("overlay").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+}
+
+function plusSlides() {
+    showSlides(slideIndex += 1);
+}
+
+function minusSlides() {
+    showSlides(slideIndex -= 1);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
 }
