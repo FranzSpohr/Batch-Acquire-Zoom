822f3c154702711471a590edd285fc9e38bc79dd
FranzSpohr <55456842+FranzSpohr@users.noreply.github.com>
2019-10-11 13:07:56 -0400

    Update reader_zoom.user.js

--- reader_zoom.user.js
+++ reader_zoom.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Slate Reader Zoom
-// @namespace    https://umich.edu/
+// @namespace    http://tampermonkey.net/
 // @version      10.11.19
 // @description  For Slate Reader. Opens a page with a highder DPI render of document. Needs Tampermonkey for Chrome or Greasemonkey for Firefox.
 // @author       University of Michigan OUA Processing (Theodore Ma)
